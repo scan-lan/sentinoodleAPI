1f1d058c0c2f841332308d18194b06d2c9a7257e
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-11-19 01:29:06 +0000

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const helmet = require("helmet")
+const cors = require("cors")
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(helmet())
+app.use(cors({
+  origin: process.env.NODE_ENV === "development" ? "*" : /sentinoodle-front-end-hx5kt35wna-nw\.a\.run\.app/
+}))
 
 app.listen(PORT, function () {
   console.log(`Express app listening on port ${PORT}`)
