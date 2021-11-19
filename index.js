c27ebafc515cd2c14886a4ab3d6e08fae6a79a09
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-11-19 01:17:40 +0000

    Add helmet

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const helmet = require("helmet")
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(helmet())
+
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`)
+})
